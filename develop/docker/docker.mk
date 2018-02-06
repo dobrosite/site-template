2166d0d7c369611ac21cfceeff8949c8c9288139
Михаил Красильников <m.krasilnikov@dobro.site>
2018-02-06 14:30:50 +0300

    Доработка взаимодействия с MySQL

--- develop/docker/docker.mk
+++ develop/docker/docker.mk
@@ -4,12 +4,31 @@ FILE_OWNER_UID := $(shell id -u)
 ## Файл конфигурации docker-compose.
 DOCKER_COMPOSE_FILE = docker-compose.dev.yml
 
+## Папка с файлами БД.
+DB_DATA_DIR = develop/docker/db/var
+
+## Имя пользователя СУБД.
+DB_USER = user
+
+## Пароль пользователя СУБД.
+DB_PASSWORD = password
+
+## Имя БД.
+DB_NAME = database
+
 ## Команда docker-compose.
 docker-compose = env FILE_OWNER_UID=$(FILE_OWNER_UID) docker-compose --file $(DOCKER_COMPOSE_FILE) $(1)
+
+## Команда mysql.
+mysql = mysql --user=$(DB_USER) --password=$(DB_PASSWORD)
 
 .PHONY: start
 start: ## Запускает контейнеры Docker.
 	$(call docker-compose,up -d)
+ifeq ($(realpath $(DB_DATA_DIR)),)
+	$(call docker-compose,exec db sh -c "while ! $(mysql) --execute='' 2> /dev/null; do echo -n '.'; sleep 1; done; echo")
+	$(MAKE) docker-init-db
+endif
 	$(MAKE) $(.DEFAULT_GOAL)
 
 .PHONY: stop
@@ -26,11 +45,8 @@ docker-rebuild: ## Пересобирает контейнеры Docker.
 .PHONY: docker-clean
 docker-clean: ## Удаляет созданные Docker файлы.
 	$(call docker-compose,down --remove-orphans)
-	-rm -rf develop/docker/db/var
+	-rm -rf $(DB_DATA_DIR)
 
 .PHONY: docker-init-db
-docker-init-db: db/database.sql  ## Загружает в БД дамп из db/database.sql.
-	$(call docker-compose,exec db sh -c "mysql --user=local --password=local local < /var/dumps/database.sql")
-
-db/database.sql:
-	$(MAKE) db-dump REMOTE=test
+docker-init-db: ## Загружает в БД дамп из db/database.sql.
+	$(call docker-compose,exec db sh -c "$(mysql) $(DB_NAME) < /var/dumps/database.sql")
